6c4dd1748606361def98a7141367b9055a32bafa
rayBlock <23381827+rayBlock@users.noreply.github.com>
2026-07-11 11:29:45 +0200

    decent v0.0.6

--- Formula/decent.rb
+++ Formula/decent.rb
@@ -1,8 +1,8 @@
 class Decent < Formula
   desc "Decent render network node supervisor CLI"
   homepage "https://github.com/decent-render/decent-render"
-  url "https://github.com/decent-render/decent-render/releases/download/v0.0.5/decent-aarch64-apple-darwin.tar.xz"
-  sha256 "776d511b688ada2fa0aa13400d1749aa89f571b25fc4d9401bca3b4a4e25906a"
+  url "https://github.com/decent-render/decent-render/releases/download/v0.0.6/decent-aarch64-apple-darwin.tar.xz"
+  sha256 "59183ef7e77811424a685a1c2b7f719a9a361bb5e4849ebbc10ba8b4e2ca6c38"
   license "Apache-2.0"
 
   depends_on arch: :arm64
